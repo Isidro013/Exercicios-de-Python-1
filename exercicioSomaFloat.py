a2bde0e6a4dd45c1300d57ce10f50a13f2e44429
Isidro Junior <iaaj013@gmail.com>
2020-02-23 23:30:21 -0300

    exercicioSomaFloat.py

---
+++ exercicioSomaFloat.py
@@ -0,0 +1,4 @@
+print("Exercicio de para Calcular a média de números Quebrados.")
+num1 = float(input("entre com o primeiro número não inteiro:"))
+num2 = float(input("entre com o segundo número não inteiro:"))
+print("O resultado da soma é %1.2f" %(num1 + num2))
