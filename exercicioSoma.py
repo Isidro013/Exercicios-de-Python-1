88c0297693ef08326a2c9ca7ed639441071c66d7
Isidro Junior <iaaj013@gmail.com>
2020-02-23 23:20:59 -0300

    exercicioSoma.py

---
+++ exercicioSoma.py
@@ -0,0 +1,4 @@
+print("Exercicio para soma de dois números Inteiros.")
+num1 = int(input("Entre com o primeiro número:"))
+num2 = int(input("Entre com o segundo número:"))
+print("A soma é %i" %(num1 + num2))
